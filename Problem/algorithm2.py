d9513cc0021eb1a9459383b31a169518c6010cab
andreshp <andreshp9@gmail.com>
2017-02-23 23:15:53 +0100

    Finish algorithm2

--- Problem/algorithm2.py
+++ Problem/algorithm2.py
@@ -1,5 +1,8 @@
 from time import time
+from copy import deepcopy
 import os
+from random import shuffle
+from random import random
 
 #### NOTA: ASEGURARSE DE QUE EXISTA EL DIRECTORIO solutions
 class AbstractSolution:
@@ -43,8 +46,10 @@ class AbstractSolution:
         self.initialSolution()
         self.saveSolution()
         self.max_score = max(0,self.scoreSolution())
-        while not self.stopSolution():
-            self.iterateSolution()
+        improved = True
+        while improved:
+            print("Trying to improve...")
+            improved = self.iterateSolution()
             score = self.scoreSolution()
             if score > self.max_score:
                 self.saveSolution(score)
@@ -144,8 +149,9 @@ class Solution(AbstractSolution):
         for _ in range(self.C):
             self.solution.append(set())
 
+        coeff = random()
         # Sort the request by rn
-        self.re.sort(key = lambda x: -x[2])
+        self.re.sort(key = lambda x: -x[2] / (1+0.001*coeff*self.v_size[x[0]]))
         # Set with the used videos
         videos = set()
         for r in self.re:
@@ -156,16 +162,91 @@ class Solution(AbstractSolution):
                        self.cache_used[pair[0]] += self.v_size[r[0]]
                        videos.add(r[0])
                        break
-                   
-    # def iterateSolution(self):
+
+        # For each cache find the used videos
+        self.cache_videos = []
+        for _ in range(self.C):
+            self.cache_videos.append({})
+        for r in self.re:
+            for pair in self.ep[r[1]].sc:
+                if r[0] not in self.cache_videos[pair[0]]:
+                    self.cache_videos[pair[0]][r[0]] = r[2]*(self.ep[r[1]].L - pair[1])
+                else:
+                    self.cache_videos[pair[0]][r[0]] += r[2]*(self.ep[r[1]].L - pair[1])
+
+        coeff2 = random()
+        self.cache_videosl = []
+        for _ in range(self.C):
+            self.cache_videosl.append([])
+        for i in range(0, self.C):
+            for x in self.cache_videos[i]:
+                self.cache_videosl[i].append((x, self.cache_videos[i][x] / (1 + coeff2*float(self.v_size[x]))))
+            #shuffle(self.cache_videosl[i])
+            self.cache_videosl[i].sort(key = lambda x: -x[1])
+
+        print("Finished first solution...")
+            
+        self.improveFirst2()
+
+        print("Improved first time...")
+            
+    def iterateSolution(self):
+        return self.improve()
 
     def improve(self):
-        for 
-    
+        improved = False
+        score = self.scoreSolutionHard()
+        for c in range(0, self.C):
+            cache = self.solution[c]
+            for video, points in self.scorecvl[c]:
+                cache.remove(video)
+                self.cache_used[c] -= self.v_size[video]
+                if self.improveFirst2(-1):
+                    print("The solution has been inproved!!!!!")
+                    return True
+                elif self.scoreSolution() < 0.5*score:
+                    cache.add(video)
+                    self.cache_used[c] += self.v_size[video]
+
+        print("We haven't improven the solution...")
+        return improved        
+        
+    def improveFirst(self, v = -1):
+        improved = False
+        score = self.scoreSolution()
+        #copy_s = deepcopy(self.solution)
+        for c in range(0,self.C):
+            cache = self.solution[c]
+            for video, points in self.cache_videosl[c]:
+                if video != v and video not in cache and self.cache_used[c] + self.v_size[video] <= self.X:
+                    cache.add(video)
+                    self.cache_used[c] += self.v_size[video]
+                    if self.scoreSolution() <= score:
+                        self.cache_used[c] -= self.v_size[video]
+                        cache.remove(video)
+        return improved
+
+    def improveFirst2(self, v = -1):
+        improved = False
+        score = self.scoreSolution()
+        #copy_s = deepcopy(self.solution)
+        for r in self.re:
+            for pair in self.ep[r[1]].sc:
+                if r[0] not in self.solution[pair[0]]:
+                    if self.cache_used[pair[0]] + self.v_size[r[0]] <= self.X:
+                       self.solution[pair[0]].add(r[0])
+                       self.cache_used[pair[0]] += self.v_size[r[0]]
+                       break                    
+                else:
+                    break
+                
     def stopSolution(self):
         return True
 
     def scoreSolution(self):
+        self.scorecv = []
+        for _ in range(0, self.C):
+            self.scorecv.append
         saved = 0
         for v,e,n in self.re:
             ld = lat = self.ep[e].L
@@ -174,6 +255,37 @@ class Solution(AbstractSolution):
                 if conn[c] < lat and v in self.solution[c]:
                     lat = conn[c]
             saved += n*(self.ep[e].L - lat)
+        return saved    
+    
+    def scoreSolutionHard(self):
+        self.scorecv = []
+        self.scorecvl = []
+        for _ in range(0, self.C):
+            self.scorecv.append({})
+            self.scorecvl.append([])
+
+        coeff = random()
+        saved = 0
+        for v,e,n in self.re:
+            ld = lat = self.ep[e].L
+            conn = self.ep[e].c
+            cb = -1
+            for c in conn:
+                if conn[c] < lat and v in self.solution[c]:
+                    lat = conn[c]
+                    cb = c
+            saved += n*(self.ep[e].L - lat)
+            if v not in self.scorecv[cb]:
+                self.scorecv[cb][v] = n*(self.ep[e].L - lat)
+            else:
+                self.scorecv[cb][v] += n*(self.ep[e].L - lat)
+
+        for c in range(0, self.C):
+            cache = self.scorecv[c]
+            for video in cache:
+                self.scorecvl.append((video, cache[video]))
+            self.scorecvl[c].sort(key = lambda x: x[1] / (1+coeff*self.v_size[x[0]]))
+                
         return saved
 
 if __name__ == "__main__":
