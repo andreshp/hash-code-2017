09903f00a3c2680f226affa1004e9e3a4118ccba
MartaAndres <martaandresarroyo@gmail.com>
2017-02-23 20:22:49 +0100

    Greedy initial solution

--- Problem/algorithm1.py
+++ Problem/algorithm1.py
@@ -19,7 +19,7 @@ class AbstractSolution:
         """
         if score is None:
             score = self.scoreSolution()
-        output = self.fname+'_'+str(score)+'.out'
+        output = os.path.join('solutions',self.fname+'_'+str(score)+'.out')
         self.writeSolution(output)
 
     def writeSolution(self, fname):
@@ -42,7 +42,7 @@ class AbstractSolution:
         """
         self.initialSolution()
         self.saveSolution(0)
-        self.max_score = max(self.max_score,self.scoreSolution())
+        self.max_score = max(0,self.scoreSolution())
         while not self.stopSolution():
             self.iterateSolution()
             score = self.scoreSolution()
@@ -72,7 +72,8 @@ class AbstractSolution:
     #### LLAMAR A ESTA PARA RESOLVER TODOS LOS FICHEROS .in
     def solveAll(self):
         """ Solve every .in file in the directory. """
-        for f in os.listdir():
+        for f in sorted(os.listdir(),
+                        key=lambda x:os.path.getsize(x)):
             if f.endswith('.in'):
                 try:
                     print('solving',f)
@@ -81,23 +82,92 @@ class AbstractSolution:
                     # Allow cut off with ^C
                     pass
 
+class EndPoint:
+    def __init__(self, L, K, connections, sorted_connections):
+        self.L = L
+        self.K = K
+        self.c = connections
+        self.sc = sorted_connections
+
+
 class Solution(AbstractSolution):
     #### IMPLEMENTAR FUNCIONES NECESARIAS AQUI
 
-    # def parseFile(self, fname):
+    def parseFile(self, fname):
+        f = open(fname)
+        self.V, self.E, self.R, self.C, self.X = map(int, f.readline().split())
+        self.v_size = list(map(int, f.readline().split()))
+        self.ep = []
 
-    # def writeSolution(self, fname):
+        # Read the endpoints
+        for i in range(0, self.E):
+            L, K = map(int, f.readline().split())
+            connections = {}
+            sorted_connections = []
+            for j in range(0, K):
+                c, Lc = map(int, f.readline().split())
+                connections[c] = Lc
+                sorted_connections.append((c,Lc))
+            sorted_connections.sort(key=lambda x: x[1])
+            self.ep.append(EndPoint(L, K, connections, sorted_connections))
 
-    # def initialSolution(self):
+
+        self.re = []
+        for i in range(0, self.R):
+            Rv, Re, Rn = map(int, f.readline().split())
+            self.re.append((Rv, Re, Rn))
+
+
+    def writeSolution(self, fname):
+        f = open(fname, 'w')
+        f.write(str(sum(1 for v in self.solution if v))+'\n')
+        for i,v in enumerate(self.solution):
+            if v:
+                f.write(str(i)+' '+' '.join(map(str, v))+'\n')
+
+    def validSolution(self):
+        for v in self.solution:
+            if sum(self.v_size[i] for i in v) > self.X:
+                return False
+        return True
+
+    def initialSolution(self):
+        # Empty solution
+        self.solution = []
+        for _ in range(self.C):
+            self.solution.append(set())
+
+        space = [self.X]*self.C
+        for v,e,n in sorted(self.re, key=lambda x: x[2], reverse=True):
+            cache = -1
+            for c,l in self.ep[e].sc:
+                if self.v_size[v] < space[c]:
+                    cache = c
+                    break
+            if cache != -1:
+                self.solution[cache].add(v)
+                space[cache] -= self.v_size[v]
 
     # def iterateSolution(self):
 
-    # def stopSolution(self):
+    def stopSolution(self):
+        return True
 
-    # def scoreSolution(self):
-
-    pass
+    def scoreSolution(self):
+        if not self.validSolution():
+            return -1
+        saved = 0
+        for v,e,n in self.re:
+            ld = lat = self.ep[e].L
+            conn = self.ep[e].c
+            for c in conn:
+                if conn[c] < lat and v in self.solution[c]:
+                    lat = conn[c]
+            saved += n*(self.ep[e].L - lat)
+        return saved
 
 if __name__ == "__main__":
     sol = Solution()
     sol.solveAll()
+    #sol.solveProblem('me_at_the_zoo.in')
+
